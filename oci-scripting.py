40a801607da5cb0e53a0f594f9f3fe903b6838ee
calvincarja <cloth.sidle0i@icloud.com>
2024-12-16 16:22:46 -0500

    build the loop to check for the special offer

--- oci-scripting.py
+++ oci-scripting.py
@@ -5,139 +5,13 @@ import random
 import pandas as pd
 import re
 import os
+import json
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import WebDriverException
 
-# build out the structure for one url
-'''
-driver = webdriver.Chrome()
 
-oci_url = "https://qa1-aws.oceaniacruises.com/caribbean-cruises/miami-to-miami-VIS241205/"
-driver.get(oci_url)
-
-time.sleep(10)
-
-body_inner_html = driver.execute_script("return document.body.innerHTML;") #entire HTML as a string, no need for .text
-
-driver.quit()
-soup = BeautifulSoup(body_inner_html, 'html5lib')
-keyword = "ultimate"
-if keyword in soup.get_text():
-    print(f"found keyword {keyword}")
-else:
-    print("keyword not found")
-'''
-
-'''
-# above was succesfull, now load into a list
-urls = []
-driver = webdriver.Chrome()
-
-oci_url = "https://qa1-aws.oceaniacruises.com/caribbean-cruises/miami-to-miami-VIS241205/"
-driver.get(oci_url)
-
-time.sleep(10)
-
-body_inner_html = driver.execute_script("return document.body.innerHTML;") #entire HTML as a string, no need for .text
-
-driver.quit()
-soup = BeautifulSoup(body_inner_html, 'html5lib')
-find_map = soup.find("div", class_ = "image").find("a") #it finds it - now extract the href
-
-if find_map:
-    print(True)
-    anchor_tag = find_map['href']
-    print(anchor_tag)
-else:
-    print(None)
-
-keyword = "image-unavailable-sm"
-if keyword in soup.get_text():
-    urls.append((oci_url, anchor_tag, "Map mising"))
-else:
-    urls.append((oci_url, anchor_tag, "Map is present"))
-
-print("complete")
-print(urls)
-'''
-
-'''
-# load the map urls into a folder on my desktop - update 10/17/2024
-def csv_to_list (csv):
-    df = pd.read_csv(csv)
-    oci_list = df['urls'].tolist()
-    return oci_list
-
-def downlaod_maps (oci_list):
-    for url in oci_list:
-        try:
-            response = requests.get(url)
-            response.raise_for_status()
-
-            # generate a filename from url
-            img_name = os.path.basename(url)
-
-'''
-
-'''
-
-# find image not avalible map urls only - update 10/14/2024
-def csv_to_list (csv):
-    df = pd.read_csv(csv)
-    oci_list = df['urls'].tolist()
-    return oci_list
-
-
-def scan_list (oci_list):
-    text_list = []
-
-    for url in oci_list:
-        try:
-            chrome_options = Options()
-            chrome_options.add_argument("--disable-extensions")
-            chrome_options.add_argument("--disable-gpu")
-            chrome_options.add_argument("--headless=new") # for Chrome >= 109
-            chrome_options.add_argument("--headless")
-            chrome_options.headless = True # also works
-            driver = webdriver.Chrome(options=chrome_options)
-            driver.get(url)
-            time.sleep(5)
-            body_inner_html = driver.execute_script("return document.body.innerHTML;")
-            driver.quit()
-            soup = BeautifulSoup(body_inner_html, 'html5lib')
-            map_soup = soup.find("div", class_ = "image").find("a")
-            map_soup_ahref = map_soup['href']
-            text_list.append((url, map_soup_ahref))
-            print(f"Prossed {url}")
-            print(text_list)
-        except WebDriverException as e:
-            print(f"An error occurred: {e}")
-        time.sleep(random.uniform(2, 4))
-    return text_list
-
-
-
-def list_to_txt (text_list, file_path):
-    try:
-        with open(file_path, 'w') as file:
-            for item in text_list:
-                file.write(str(item) + '\n')
-        return True, f"data sucessfully saved to {file_path}" # returns two items
-    except IOError as e:
-        return False, f"an error as occurred while writing to the file: {e}"
-
-def master ():
-    oci_list = csv_to_list('/Users/calvinpineda/Downloads/collection-map-urls-segment.csv')
-    text_list = scan_list(oci_list)
-    print('complete')
-
-master()
-
-
-'''
-
-# 11/8/2024 - updated version
+# 11/8/2024 - loading maps from the website
 '''
 improved version ** 11/8/2024
 def process_url (url, keyword):
@@ -191,7 +65,7 @@ def master ():
 master()
 
 '''
-
+'''
 # 11/8/2024 - updated version with multiple csv files
 
 def process_url (url, keyword):
@@ -269,7 +143,7 @@ master()
 
 
 
-
+'''
 '''
 def csv_to_list (csv):
     df = pd.read_csv(csv)
@@ -312,65 +186,29 @@ def master ():
 master()
 '''
 
+# 12/16/24 - use HTTP request on API url
 
-'''
-if keyword in soup.get_text():
-    urls.append((oci_url, element_html, "image-unavailable present"))
-        print(f"Processed {url} - voyage map not found")
-          #else:
-            #store_urls.append(url, element_html, "voyage map present")
-            #print(f"Processed {url} - voyage map found")
+url = "https://qa1-aws.oceaniacruises.com/api/cruise-details/v1/special-offers"
 
-'''
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+}
 
-"""
-inner_text = elements.text
-print(inner_text)
+loop_breaker = 0
+while loop_breaker != 1:
+    response = requests.get(url, headers=headers)
+    dict_response = response.json()
 
-inner_html = elements.decode_contents()
-print(inner_html)
-"""
+    for x in dict_response:
+        print(x["id"])
+        if x["id"] == "oceania-cruises":
+            loop_breaker += 1
+            print("Loop broken")
+            break
+    current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    print(f"Current time: {current_time}")
+    time.sleep(60)
+
+print("Loop broken successfully")
 
 
-"""
-
-def extract (url, keyword):
-    store_bad_urls = []
-    store_urls = []
-    try:
-          response_url = requests.get(url)
-          response_url.raise_for_status()
-          response_url_text = response_url.text
-          response_url_text_readble = BeautifulSoup(response_url_text, 'html5lib')
-          print(response_url_text_readble)
-          elements = response_url_text_readble.find("div", element_class = "image") # only one image so lets try find (not find_all)
-          print(elements)
-          print('10')
-          #for element in elements:
-            #print("Text:", element.text)
-            #print("HTML Content:", element.decode_contents())
-          #if keyword in response_url_text_readble:
-            #store_urls.append((url, element_html, "image-unavailable present"))
-            #print(f"Processed {url} - voyage map not found")
-          #else:
-            #store_urls.append(url, element_html, "voyage map present")
-            #print(f"Processed {url} - voyage map found")
-        
-    except requests.exceptions.RequestException as e:
-        store_bad_urls.append((url, str(e)))
-        print(f"Error Processing {url}: {e}")
-
-extract(oci_url, keyword)
-
-"""
-'''
-# loading target
-driver = webdriver.Chrome()
-
-# Open a test website to verify
-driver.get("https://www.target.com")
-
-time.sleep(5)
-driver.quit
-
-'''
